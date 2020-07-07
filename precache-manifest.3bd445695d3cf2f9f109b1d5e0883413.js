267dc0e8e536ea49f6549c757993e99b689073e4
Sourish Ghosh <sourish.njs@gmail.com>
2020-07-07 23:11:03 +0530

    Updates

---
+++ precache-manifest.3bd445695d3cf2f9f109b1d5e0883413.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f4d0fdedb921f18cd70527084f4a9e",
+    "url": "/personalchef/index.html"
+  },
+  {
+    "revision": "122dd7fe479ac26ad73a",
+    "url": "/personalchef/static/css/main.99350e71.chunk.css"
+  },
+  {
+    "revision": "23443a64999c97d72c6d",
+    "url": "/personalchef/static/js/2.59a4b0a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personalchef/static/js/2.59a4b0a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "122dd7fe479ac26ad73a",
+    "url": "/personalchef/static/js/main.c617518c.chunk.js"
+  },
+  {
+    "revision": "5efb9f6b5f84d417ccb4",
+    "url": "/personalchef/static/js/runtime-main.166ccd17.js"
+  }
+]);
